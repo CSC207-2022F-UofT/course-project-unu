28143af73bd5c74abfd5857d988cb3ba07147d47
Paul Tran <pl.tran@mail.utoronto.ca>
2022-10-31 15:54:58 -0400

    player instance variables and getter/setter methods

---
+++ src/Entities/Player.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private String name;
+    private List<Card> hand;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<Card> getHand() {
+        return this.hand;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHand(List<Card> hand) {
+        this.hand = hand;
+    }
+}
