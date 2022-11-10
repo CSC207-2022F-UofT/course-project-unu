5e948d0a13a7368e0405b341c677f77e914c8417
amdsuperfan <yujun.wu@mail.utoronto.ca>
2022-11-09 20:39:22 -0500

    UserProfile and RecordGame Class

---
+++ src/main/java/UserProfilePage/UserProfile.java
@@ -0,0 +1,25 @@
+package UserProfilePage;
+
+public class UserProfile{
+    public String name;
+    public int GamesPlayed;
+    public int GamesWon;
+    public float WinRate;
+
+
+    public void UserProfile(String name){
+        //Creates a new player
+        this.name = name;
+        this.GamesPlayed = 0;
+        this.GamesWon = 0;
+    }
+
+    public void UserProfile(boolean outcome){
+        //takes a input of win/loss and records a game
+        this.GamesPlayed ++;
+        if (outcome){
+            this.GamesWon ++;
+        }
+        this.WinRate = this.GamesWon / this.GamesPlayed;
+    }
+}
