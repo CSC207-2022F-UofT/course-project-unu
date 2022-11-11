e3d6830b229cc4bc24ad1db53537016ac040d64d
SarahX43 <sarah.xie@mail.utoronto.ca>
2022-11-11 15:08:29 -0500

    StartPage: a raw start page UI with no functionality linked yet. GameManager: contains the main method that interacts with the user

---
+++ src/main/java/UI/GameManager.java
@@ -0,0 +1,18 @@
+package UI;
+
+/**
+ * the Main class that the users interact with
+ */
+public class GameManager {
+
+    StartPage ui = new StartPage(this);
+
+    public static void main(String[] args) {
+
+        new GameManager();
+    }
+    
+    public GameManager() {
+
+    }
+}
