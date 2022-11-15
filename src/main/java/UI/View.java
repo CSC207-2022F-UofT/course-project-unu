5c9091e103cc213d0cb0cd910a16d42a57662666
SarahX43 <sarah.xie@mail.utoronto.ca>
2022-11-15 07:40:50 -0500

    Controller and View interface methods behaviour

--- src/main/java/UI/View.java
+++ src/main/java/UI/View.java
@@ -1,25 +1,15 @@
 package UI;
 
+import javax.swing.*;
+
 /**
  * View Interface with methods that the Presenter calls to pass values for display
  */
 public interface View {
 
-    void createMainField();
+    void updateCardDrawn();
 
-    void createBackground(int panelNum, String bgFileName);
+    void updateCardPlayed();
 
-//    void createStartPage();
-//
-//    void createGamePage();
-//
-//    /**
-//     * update the card deck of the player in the view
-//     */
-//    void updateCardDeck();
-//
-//    /**
-//     * display a choose colour page with three buttons after a wildCard is played
-//     */
-//    void chooseColour();
+
 }
