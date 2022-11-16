fb4b2b65d828961bcf32235b81d34ed1c1b10278
bober <kovalsviatic@gmail.com>
2022-11-15 23:03:02 -0500

    add comments to the methods

--- src/main/java/TeamMode/Team.java
+++ src/main/java/TeamMode/Team.java
@@ -5,24 +5,51 @@ import entities.Player;
 import java.util.List;
 
 public class Team {
+    /**
+     * list of the team players
+     */
     private List<Player> teamPlayers;
     private String name;
     public Team(){
     }
+
+    /**
+     * creates a Team object
+     * @param name name of the team
+     */
     public Team(String name){
         this.name = name;
     }
 
+    /**
+     * returns name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * sets new name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
+
+    /**
+     * return the all players in the team
+     * @return
+     */
     public List<Player> getTeamPlayers(){
         return teamPlayers;
     }
+
+    /**
+     * adds new team player
+     * @param player - new player
+     * @return
+     */
     public boolean addTeamPlayer(TeamPlayer player){
         if(teamPlayers.contains(player)){
             return false;
--- src/main/java/TeamMode/TeamPlayer.java
+++ src/main/java/TeamMode/TeamPlayer.java
@@ -4,26 +4,43 @@ import entities.RealPlayer;
 
 public class TeamPlayer extends RealPlayer {
         private Team teamOfPlayer;
+
+        /**
+         * creates a team player
+         * @param name
+         * @param team
+         */
         public TeamPlayer(String name, Team team){
                 super(name);
                 teamOfPlayer= team;
         }
 
+        /**
+         * return team name
+         * @return
+         */
         public String getTeamName() {
                 return teamOfPlayer.getName();
         }
-
+        /**
+         * sets team name
+         * @return
+         */
         public void setTeamName(String name) {
                 this.teamOfPlayer.setName(name);
         }
 
-        public Team getTeamOfPlayer() {
-                return teamOfPlayer;
-        }
-
+        /**
+         * sets team
+         * @return
+         */
         public void setTeamOfPlayer(Team teamOfPlayer) {
                 this.teamOfPlayer = teamOfPlayer;
         }
+        /**
+         * returns team
+         * @return
+         */
         public Team getTeam(){
                 return this.teamOfPlayer;
         }
