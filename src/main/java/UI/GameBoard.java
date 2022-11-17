1a25c7a54ae2db8cf995807d9b631a6b7ab3e927
SarahX43 <sarah.xie@mail.utoronto.ca>
2022-11-16 19:29:06 -0500

    Created bot player labels on the game board

--- src/main/java/UI/GameBoard.java
+++ src/main/java/UI/GameBoard.java
@@ -1,6 +1,7 @@
 package UI;
 
 import game.Game;
+import interfaceAdapters.Controller;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,44 +15,141 @@ import java.awt.event.MouseListener;
  */
 public class GameBoard{
 
-    //private final Controller c;
+    Controller c;
+    ViewMethods vm;
 
-    private final JLabel label;
+    JFrame window;
+    JPanel gamePanel;
+    JLabel gameLabel;
 
     public GameBoard() {
-        final JFrame frame = new JFrame();
-        frame.setSize(200, 100);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        label = new JLabel();
-        frame.add(label);
+        generateScreen();
 
-        // TODO: ask if we can have a default Game initializer
-        //Game g = new Game();
-        //c = new Controller(g)
-
-        frame.setVisible(true);
     }
 
+    public void generateScreen() {
+        //create a main field
+        window = new JFrame();
+        window.setSize(1000, 700);
+        window.setTitle("Game Board");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //close the windows properly
+        window.setLayout(null);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        //set background
+        gamePanel = new JPanel();
+        gamePanel.setBounds(0, 0, 1000, 700);
+        gamePanel.setLayout(null);
+        window.add(gamePanel);
+        gameLabel = new JLabel();
+        gameLabel.setBounds(0, 0, 1000, 700);
+        ImageIcon startBG = new ImageIcon(this.getClass().getResource("/gameBoardBG.png"));
+        gameLabel.setIcon(startBG);
+        gamePanel.add(gameLabel);
+
+        createPlayers(gameLabel);
+    }
+
+    public void createPlayers(JLabel bg) {
+        JLabel bot1 = new JLabel();
+        ImageIcon bot1Icon = new ImageIcon(this.getClass().getResource("/RedBot.png"));
+        Image bot1Img = bot1Icon.getImage();
+        Image adjustedBot1 = bot1Img.getScaledInstance(60, 70, Image.SCALE_SMOOTH);
+        bot1.setBounds(100, 100, 60, 70);
+        bot1Icon = new ImageIcon(adjustedBot1);
+        bot1.setIcon(bot1Icon);
+
+        JLabel bot2 = new JLabel();
+        ImageIcon bot2Icon = new ImageIcon(this.getClass().getResource("/StripeBot.png"));
+        Image bot2Img = bot2Icon.getImage();
+        Image adjustedBot2 = bot2Img.getScaledInstance(60, 70, Image.SCALE_SMOOTH);
+        bot2.setBounds(400, 100, 60, 70);
+        bot2Icon = new ImageIcon(adjustedBot2);
+        bot2.setIcon(bot2Icon);
+
+        JLabel bot3 = new JLabel();
+        ImageIcon bot3Icon = new ImageIcon(this.getClass().getResource("/WhiteBot.png"));
+        Image bot3Img = bot3Icon.getImage();
+        Image adjustedBot3 = bot3Img.getScaledInstance(60, 70, Image.SCALE_SMOOTH);
+        bot3.setBounds(700, 100, 60, 70);
+        bot3Icon = new ImageIcon(adjustedBot3);
+        bot3.setIcon(bot3Icon);
+        bg.add(bot1);
+        bg.add(bot2);
+        bg.add(bot3);
+
+        JLabel textMessage = new JLabel("Hello World");
+        textMessage.setBackground(Color.black);
+        bg.add(textMessage);
 
 
+        JLabel bot4 = new JLabel();
+        bot4.setBounds(200, 400, 100, 100);
+
+        bg.add(bot4);
+
+        //JLabel card1 = createCardLabel(500, 500, "1-red");
+        //bg.add(card1);
+        JLabel cardTest = new JLabel();
+        cardTest.setText("1");
+        cardTest.setIcon(bot2Icon);
+        cardTest.setBounds(500, 500, 50, 100);
+        cardTest.setBackground(Color.red);
+        bg.add(cardTest);
+
+    }
 
     /**
      * number card
      * @param cardX
      * @param cardY
-     * @param colour
-     * @param number
+     * @param cardStr
+     * I will give a string that represents the card, which will be in the form of "value-colour"
+     * For example: number card "1-red"
+     * plus four card "+4"
+     * plus two card "+2-red"
+     * reverse card "R-red"
+     * skip card "S-red"
+     * wild card "W"
      */
-    public void createCardLabel(int cardX, int cardY, Color colour, int number) {
-        JLabel cardLabel = new JLabel();
-        cardLabel.setBounds(cardX, cardY, 50, 100);
-        cardLabel.setText(String.valueOf(number));
-        cardLabel.setBackground(colour);
+    public JLabel createCardLabel(int cardX, int cardY, String cardStr) {
+        JLabel card = new JLabel();
+        card.setBounds(cardX, cardY, 50, 100);
+        String cardText = "";
+        Color cardColor = Color.white;
+        if (cardStr.equals("+4")) {
+            cardText = cardStr;
+            cardColor = Color.black;
+        } else if (cardStr.equals("W")) {
+            cardText = cardStr;
+            cardColor = Color.black;
+        } else {
+            String[] splitedCard = cardText.split("-");
+            String text = splitedCard[0];
+            String colour = splitedCard[1];
+            cardText = text;
+            switch (colour) {
+                case "red":
+                    cardColor = Color.red;
+                    break;
+                case "yellow":
+                    cardColor = Color.yellow;
+                    break;
+                case "green":
+                    cardColor = Color.green;
+                    break;
+                case "blue":
+                    cardColor = Color.blue;
+                    break;
+            }
+        }
 
-        //TODO:
-        //gamePanel[2].add(cardLabel);
+        card.setText(cardText);
+        card.setBackground(cardColor);
 
+        return card;
     }
 
     /**
