6e929502f850988294654d3fa484539d26eca3cb
Thompson Li <thompson.li@mail.utoronto.ca>
2022-11-17 17:17:52 -0500

    Created Interfaces

---
+++ src/main/java/interfaceAdapters/Presenter_Interface.java
@@ -0,0 +1,8 @@
+package interfaceAdapters;
+
+/**
+ * An interface of methods that lower level objects can freely interface with.
+ */
+public interface Presenter_Interface {
+    void requestNewColour();
+}
