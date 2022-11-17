bd1a3feadf5b8cce9b7b319a8dd88352aebfb81e
Thompson Li <thompson.li@mail.utoronto.ca>
2022-11-17 17:20:32 -0500

    Created a presenter interface in Game

--- src/main/java/cards/WildCard.java
+++ src/main/java/cards/WildCard.java
@@ -15,21 +15,14 @@ public class WildCard extends Card {
      */
     @Override
     public void playedEffect(Game game) {
-        ChangeColour useCase = new ChangeColour_Interface();
-        super.setColour(useCase.getNewColour());
+        new ColourChange_UseCaseInteractor(game.getPresenter()).requestColourChange();
         /*
-        Make this method call a use case. The use case will handle changing the colour of this card.
-        The use case will ask the interface to provide an input which it will return to this method.
-        The main issue with this is that we'll break clean architecture.
-        This entity would be calling a use case/an interface. Even if we made this abstract,
-        we'd need to create an object somewhere (Maybe we can still do this).
+        new RequestColourChange().requestColourChange();
+        Get this to call the use case while preserving Clean Architecture.
 
-        Alternatively, we could have the draw system handle this code,
-        getting the interface to change colour whenever a Wild is played,
-        but that means we loose abstraction (we need to check the type of the card).
-
-         */
-        // TODO: Implement with User Inputs - super.setColour( /* User input */ );
+        Current Flow of Code:
+        WildCard > RequestColourChange (Use Case) > Presenter (Interface) > View (Interface)
+        */
     }
 
     /**
