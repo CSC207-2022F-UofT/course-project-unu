6a44f5419971d08d64b2302b980fea2f0a3aeb04
SarahX43 <sarah.xie@mail.utoronto.ca>
2022-11-18 15:07:34 -0500

    added viewmethod clas

--- src/main/java/UI/ViewMethods.java
+++ src/main/java/UI/ViewMethods.java
@@ -24,7 +24,12 @@ public class ViewMethods implements View{
         str[4] = "R-blue";
         str[5] = "S-yellow";
         str[6] = "0-red";
-        displayAvailableCards(str);
+        updateAvailableCards(str);
+        updateMyLastPlayedCard("R-green");
+        updateLastCardPlayed("S-blue");
+        updateBot1Card("+4");
+        updateBot2Card("W");
+        updateBot3Card("9-red");
     }
 
     @Override
@@ -32,61 +37,44 @@ public class ViewMethods implements View{
         gameBoard = new GameBoard();
     }
 
-    public void displayAvailableCards(String[] cardList) {
+    @Override
+    public void updateLastCardPlayed(String card) {
+        gameBoard.updateLastPlayed(card);
+    }
+
+    @Override
+    public void updateMyLastPlayedCard(String card) {
+        gameBoard.updatePlayer1Card(card);
+    }
+
+    @Override
+    public void updateBot1Card(String card) {
+        gameBoard.updatePlayer2Card(card);
+    }
+
+    @Override
+    public void updateBot2Card(String card) {
+        gameBoard.updatePlayer3Card(card);
+    }
+
+    @Override
+    public void updateBot3Card(String card) {
+        gameBoard.updatePlayer4Card(card);
+    }
+
+    public void updateAvailableCards(String[] cardList) {
         JLabel[] cardLabels = new JLabel[cardList.length];
         for (int i=0; i<cardList.length; i++) {
-            System.out.println(i);
             int cardX = 260 + 70*(i / 2);
-            System.out.println(cardX);
             int cardY = 440 + 90*(i % 2);
-            System.out.println(cardY);
             cardLabels[i] = gameBoard.createCardLabel(cardX, cardY, 60, 80, cardList[i]);
         }
         gameBoard.displayAvailableCards(cardLabels);
     }
 
-
-    @Override
-    public void updateLastCardPlayed() {
-
-    }
-
-    @Override
-    public void updateCardDrawn() {
-
-    }
-
-    @Override
-    public void updateCardPlayed() {
-
-    }
-
-    @Override
-    public void updateMyLastPlayedCard() {
-
-    }
-
-    @Override
-    public void updateBot1Card() {
-
-    }
-
-    @Override
-    public void updateBot2Card() {
-
-    }
-
-    @Override
-    public void updateBot3Card() {
-
-    }
-    
-    
-
     @Override
     public void requestColorChange() {
 
     }
-
 
 }
