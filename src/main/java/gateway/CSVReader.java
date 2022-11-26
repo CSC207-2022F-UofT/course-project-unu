d45c8f86879f578227aafa255b2961bccfdb87cf
Sean Grindal <sean37gl@gmail.com>
2022-11-26 15:59:00 -0500

    some new methods for setting the csv

--- src/main/java/gateway/CSVReader.java
+++ src/main/java/gateway/CSVReader.java
@@ -1,13 +1,8 @@
 package gateway;
 import java.io.*;  
-import java.util.Scanner;  
 
 public class CSVReader {
   BufferedReader csvReader;
-
-  public static void main(String[] args) {
-    new CSVReader("db/stats.csv");
-  }
 
   public CSVReader(String filename) {
     try {
@@ -17,27 +12,82 @@ public class CSVReader {
     }
   }
 
-  public void listCSVLines() {
-    String row;
+  private int findCol(String colName) {
+    if (this.csvReader == null) {
+      return -1;
+    }
+
     try {
-      while ((row = this.csvReader.readLine()) != null) {
-        String[] data = row.split(",");
-        System.out.println(data[0]);
+      String[] headings = this.csvReader.readLine().split(", ");
+      for (int i = 0; i < headings.length; i++) {
+        if (headings[i].equals(colName)) {
+          return i;
+        }
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
+
+    return -1;
   }
 
-  public int getWins() {
+  public int getColValue(String colName) {
     if (this.csvReader == null) { 
       return 0;
+    }
+
+    try {
+      int colOfVal = this.findCol(colName);
+
+      if (colOfVal == -1) {
+        return 0;
+      }
+
+      return Integer.parseInt(this.csvReader.readLine().split(", ")[colOfVal]);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
     return 0;
   }
 
-  public int getTotalGames() {
+  public void setColValue(String colName, int newValue) {
+    if (this.csvReader == null) { 
+      return;
+    }
 
+    try {
+      int colOfVal = this.findCol(colName);
+
+      if (colOfVal == -1) {
+        return;
+      }
+
+      String[] data = this.csvReader.readLine().split(", ");
+      data[colOfVal] = Integer.toString(newValue);
+      this.csvReader.close();
+      this.csvReader = new BufferedReader(new FileReader("db/stats.csv"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public int getTotalGames() {
+    return getColValue("Total Games");
+  }
+
+  public int getTotalWins() {
+    return getColValue("Total Wins");
+  }
+
+  public void intIncreaseTotalWins(int amount) {
+    this.setColValue("Total Wins", this.getTotalWins() + amount);
+  }
+
+  public void recordNewGame(boolean win) {
+    this.setColValue("Total Games", this.getTotalGames() + 1);
+    if (win) {
+      this.intIncreaseTotalWins(1);
+    }
   }
 }
