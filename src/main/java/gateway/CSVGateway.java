b17740dc8778e13e42feccfaa4575f45836fe402
Sean Grindal <sean37gl@gmail.com>
2022-11-27 14:15:41 -0500

    complete gateway

---
+++ src/main/java/gateway/CSVGateway.java
@@ -0,0 +1,137 @@
+package gateway;
+import java.io.*;  
+
+public class CSVGateway implements DBGateway {
+  BufferedReader csvReader;
+  String filename;
+  String[] headings;
+  String[] data;
+
+  public CSVGateway(String filename) {
+    this.filename = filename;
+    this.readInNewFile();
+  }
+
+  /**
+   * Read data from the file
+   * Returns true if successful, false if not
+   * @return boolean
+   */
+  private boolean readInNewFile() {
+    try {
+      this.csvReader = new BufferedReader(new FileReader(this.filename));
+      this.headings = this.csvReader.readLine().split(",");
+      this.data = this.csvReader.readLine().split(",");
+
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+
+      return false;
+    }
+  }
+
+  /**
+   * Saves the current headings and data to the CSV file
+   * Returns true if successful, false if not
+   * @returns boolean
+   */
+  private boolean writeToFile() {
+    try {
+      FileWriter csvWriter = new FileWriter(this.filename);
+      csvWriter.append(String.join(",", this.headings));
+      csvWriter.append("\n");
+      csvWriter.append(String.join(",", this.data));
+      csvWriter.flush();
+      csvWriter.close();
+
+      return true;
+    } catch (IOException e) {
+      e.printStackTrace();
+      
+      return false;
+    }
+  }
+
+  /**
+   * Finds the col index with given heading name
+   * @param String colName
+   */
+  public int findColIndexByHeading(String colName) {
+    if (this.headings == null) {
+      return -1;
+    }
+
+    for (int i = 0; i < this.headings.length; i++) {
+      if (this.headings[i].equals(colName)) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  /**
+   * Gets the value of the first row with a given heading name
+   * @param String colName
+   * @return Int
+   */
+  public String getColValue(String colName) {
+    int colOfVal = this.findColIndexByHeading(colName);
+    if (colOfVal == -1) {
+      return "0";
+    }
+
+    return this.data[colOfVal];
+  }
+
+  /**
+   * Sets the value of the first row with a given heading name
+   * @param String colName
+   * @param Int newValue
+   */
+  public void setColValue(String colName, String newValue) {
+    int colOfVal = this.findColIndexByHeading(colName);
+    if (colOfVal == -1) {
+      return;
+    }
+
+    this.data[colOfVal] = newValue;
+    this.writeToFile();
+  }
+
+  /**
+   * Get the total number of games played
+   * @param boolean isWin
+   */
+  public int getTotalGames() {
+    return Integer.parseInt(getColValue("Total Games"));
+  }
+
+  /**
+   * Get the total number of wins
+   */
+  public int getTotalWins() {
+    return Integer.parseInt(getColValue("Total Wins"));
+  }
+
+  /**
+   * Increase the total number of wins by the specified amount 
+   * @param amount
+   */
+  public void increaseTotalWins(int amount) {
+    this.setColValue("Total Wins", Integer.toString(this.getTotalWins() + amount));
+  }
+
+  /**
+   * Record a new game and increment the total number of games played by 1
+   * If the game was a win, increment the total number of wins by 1
+   * @param isWin
+   */
+  public void recordNewGame(boolean isWin) {
+    this.setColValue("Total Games", Integer.toString(this.getTotalGames() + 1));
+    if (isWin) {
+      this.increaseTotalWins(1);
+    }
+  }
+}
