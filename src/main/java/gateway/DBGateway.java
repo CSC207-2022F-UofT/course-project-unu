5e752e0250edaeaad27b3321432db7e3338f2ed2
Sean Grindal <sean37gl@gmail.com>
2022-11-27 14:18:57 -0500

    cd

--- src/main/java/gateway/DBGateway.java
+++ src/main/java/gateway/DBGateway.java
@@ -1,9 +1,20 @@
 package gateway;
 
 public interface DBGateway {
+  /**
+   * Record a new game and increment the total number of games played by 1
+   * If the game was a win, increment the total number of wins by 1
+   * @param isWin
+   */
   public void recordNewGame(boolean isWin);
 
+  /**
+   * Get the total number of wins
+   */
   public int getTotalGames();
 
+  /**
+   * Get the total number of games played
+   */
   public int getTotalWins();
 }
