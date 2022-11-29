e53d8abe4ca197f8b70bbe99d343273b56b1ebdf
SarahX43 <sarah.xie@mail.utoronto.ca>
2022-11-29 18:59:41 -0500

    Fixed intellij warning and added documentation for StartPage and UIComponent

--- src/main/java/UI/UIComponent.java
+++ src/main/java/UI/UIComponent.java
@@ -4,14 +4,29 @@ import interfaceAdapters.Controller;
 
 import javax.swing.*;
 
+/**
+ * An abstract super class of all the UI component classes
+ * defines methods share by all UI components like createMainField() and generateScreen()
+ */
 public abstract class UIComponent {
 
     Controller c;
 
+    /**
+     *
+     * @param c The controller of this game that each UI component interacts with, takes the input from UI
+     */
     public UIComponent(Controller c) {
         this.c = c;
     }
 
+    /**
+     * create the main window of this page
+     * @param screenName the page name displayed at the top of the window
+     * @param width window width
+     * @param height window height
+     * @return the initialized main window
+     */
     public JFrame createMainField(String screenName, int width, int height) {
         JFrame window = new JFrame();
         window.setSize(width, height);
@@ -23,6 +38,9 @@ public abstract class UIComponent {
         return window;
     }
 
+    /**
+     * the method that every UI component should implement
+     */
     public abstract void generateScreen();
 
 }
