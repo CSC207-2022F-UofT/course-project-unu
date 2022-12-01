6de866b90d85c489d42e14a6f7bc72bef407e520
SarahX43 <sarah.xie@mail.utoronto.ca>
2022-12-01 13:52:23 -0500

    cleaned repeated create card codes

--- src/main/java/UI/PlayCardWindow.java
+++ src/main/java/UI/PlayCardWindow.java
@@ -7,43 +7,27 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class PlayCardWindow {
+public class PlayCardWindow extends UIComponent {
 
     JFrame window;
     JPanel playPanel;
-    Controller c;
 
     public PlayCardWindow(Controller c) {
-        //TODO: figure out why can the parameter passed be null
-        this.c = c;
-        this.window = new JFrame();
+        super(c);
         generateScreen();
     }
 
     public void generateScreen() {
 
         //create a main field
+        window = super.createMainField("Choose Cards", 250, 200);
         window.setSize(250, 200);
-        window.setTitle("Choose Cards");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //close the windows properly
-        window.setLayout(null);
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
 
         //set background
         playPanel = new JPanel();
         playPanel.setBounds(0, 0, 250, 200);
         playPanel.setLayout(null);
         window.add(playPanel);
-        String[] str = new String[7];
-        str[0] = "W";
-        str[1] = "+4";
-        str[2] = "1-red";
-        str[3] = "+2-green";
-        str[4] = "R-blue";
-        str[5] = "S-yellow";
-        str[6] = "0-red";
-        createAvailableMoves(str);
     }
 
     public void createAvailableMoves(String[] availableMoves) {
@@ -56,39 +40,17 @@ public class PlayCardWindow {
         }
     }
 
-
     public JButton createCardButtons(int cardX, int cardY, int width, int height, String cardStr) {
 
         JButton card = new JButton();
         card.setBounds(cardX, cardY, width, height);
-        card.setForeground(Color.white);
-        String cardText = "";
-        Color cardColor = Color.white;
-        if (cardStr.equals("+4")) {
-            cardText = cardStr;
-            cardColor = Color.black;
-        } else if (cardStr.equals("W")) {
-            cardText = cardStr;
-            cardColor = Color.black;
+
+        String cardText = super.getCardText(cardStr);
+        Color cardColor = getCardColor(cardStr);
+        if (cardColor == Color.yellow) {
+            card.setForeground(Color.black);
         } else {
-            String[] splitedCard = cardStr.split("-");
-            String text = splitedCard[0];
-            String colour = splitedCard[1];
-            cardText = text;
-            switch (colour) {
-                case "red":
-                    cardColor = Color.red;
-                    break;
-                case "yellow":
-                    cardColor = Color.yellow;
-                    break;
-                case "green":
-                    cardColor = Color.green;
-                    break;
-                case "blue":
-                    cardColor = Color.blue;
-                    break;
-            }
+            card.setForeground(Color.white);
         }
 
         card.setText(cardText);
