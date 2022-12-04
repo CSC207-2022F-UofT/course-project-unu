c62b8ce5c2bdd477c557e61b73224f144ebc3aab
Paul Tran <pl.tran@mail.utoronto.ca>
2022-12-04 18:09:27 -0500

    changed checkWin

--- src/main/java/useCases/CheckWin.java
+++ src/main/java/useCases/CheckWin.java
@@ -1,9 +1,19 @@
 package useCases;
 
 import entities.GameState;
+import entities.Player;
+import java.util.List;
 
 public class CheckWin {
     public boolean checkGameOver(GameState gameState) {
-        return gameState.getPlayers().get(gameState.getToMove()).getHand().isEmpty();
+        List<Player> players = gameState.getPlayers();
+
+        for (Player player: players) {
+            if (player.getHand().isEmpty()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
