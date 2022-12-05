9cf09e30652e9b13b9709c607e349bebefb626eb
Paul Tran <pl.tran@mail.utoronto.ca>
2022-12-04 19:46:15 -0500

    regularPlayerList initializes botLevels correctly

--- src/main/java/interfaceAdapters/Controller.java
+++ src/main/java/interfaceAdapters/Controller.java
@@ -115,7 +115,7 @@ public class Controller {
         Player player = new RealPlayer(playerNames.get(0));
         playerList.add(player);
         for (int k = 1; k < playerNames.size(); k++) {
-            player = new BotPlayer(playerNames.get(k), botLevels.get(k)) {// HERE BOT PLAYER
+            player = new BotPlayer(playerNames.get(k), botLevels.get(k - 1)) {// HERE BOT PLAYER
             };
             playerList.add(player);
         }
