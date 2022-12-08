42306287aba5a2ed8702b53dfab9f3f899cd528e
SarahX43 <sarah.xie@mail.utoronto.ca>
2022-12-08 16:59:43 -0500

    Final documentation

--- src/main/java/UI/ScoreBoard.java
+++ src/main/java/UI/ScoreBoard.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * UI component, extends the abstract class UIComponent
+ * displays player ranking
+ */
 public class ScoreBoard extends UIComponent{
 
     JFrame window;
@@ -11,6 +15,11 @@ public class ScoreBoard extends UIComponent{
     String rankType;
     List<String> players;
 
+    /**
+     * Constructor
+     * @param rankType rank type
+     * @param players sorted list of players
+     */
     public ScoreBoard(String rankType, List<String> players) {
         super();
         this.rankType = rankType;
@@ -18,6 +27,9 @@ public class ScoreBoard extends UIComponent{
         generateScreen();
     }
 
+    /**
+     * generate the screen of scoreboard
+     */
     public void generateScreen() {
         //create a main field
         window = super.createMainField("Player Ranking by " + rankType, 500, 500);
@@ -25,22 +37,31 @@ public class ScoreBoard extends UIComponent{
         //set up background
         scoreBoardLabel = super.setBackground(window, 500, 500);
 
+        //column titles
         JLabel columns = new JLabel("Rank   Player");
         columns.setBounds(100, 50, 300, 50);
         columns.setForeground(Color.white);
         columns.setFont(new Font("Monospace", Font.PLAIN, 40));
         scoreBoardLabel.add(columns);
 
+        //add each player
         for (int i=0; i< players.size(); i++) {
             addPlayer(scoreBoardLabel, i+1, players.get(i));
         }
 
+        //exit button
         JButton exitButton = new JButton("Exit");
         exitButton.setBounds(400, 400, 50, 50);
         exitButton.addActionListener(e -> window.setVisible(false));
         scoreBoardLabel.add(exitButton);
     }
 
+    /**
+     * helper method, add the player text label to the page
+     * @param bg background label
+     * @param rank player rank
+     * @param player player name
+     */
     public void addPlayer(JLabel bg, int rank, String player) {
         JLabel playerRank = new JLabel(rank + "         " + player);
         playerRank.setBounds(135, 70+rank*70, 300, 50);
